c0b73ef450f8d92b162ce6babb2e4ed2b08671e4
SK Jiyad <100613008+ZRX-SIGMA@users.noreply.github.com>
2024-12-01 20:36:30 +0000

    -

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId=24424
+let accountEmail="alpha@mail.com"
+var accountPass="alpha"
+accountCity="Asansol"
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+// accountId=736
+accountEmail="kdz@kd.com"
+accountPass="jbc"
+accountCity="DGP"
+console.table([accountId,accountEmail,accountPass,accountCity])
